7dc3488d5e55115a1332e59d8e3f1dd126855bb4
Lewis Howles <lewishowles@users.noreply.github.com>
2026-01-14 16:32:23 +0000

    Run Chrome in headless mode

--- routes/cineworld/films/index.js
+++ routes/cineworld/films/index.js
@@ -29,6 +29,7 @@ export default async (request, response) => {
 async function loadBranchData(searchData) {
 	const browser = await puppeteer.launch({
 		args: ["--no-sandbox", "--disable-setuid-sandbox"],
+		headless: true,
 	});
 
 	const page = await browser.newPage();
